926c142a82398b5eb14564a0f4366ba0d1d16374
geumkyeong <geumkyeong@github.com>
2018-10-24 19:33:41 +0900

    edit 04

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 var wrap = document.getElementById('wrap');
-var btn = document.querySelector('.btn'); //더보기 button.
-var start = 0;
-var end = 4; //최대 범주번호
-
-function btnClick() { //더보기
-    start += 4;
-    end = start + 4;
-
-    if(!(end == 32)){ getUrlData(radio); }
-    else {
-      getUrlData(radio);
-      btn.removeEventListener('click', btnClick);
-      btn.remove();//end에 다다르면 더보기 없애기
-    }
-
-}
-
-btn.addEventListener('click', btnClick); //이벤트 리스너 추가
 
 function radio(json) {
   var str = '';
 
-  for(var i = start;  i < end; i++){
+  for(var i = 0;  i < 32; i++){
         var category = json.data[i].category; //범주
         var n = json.data[i].num;             //범주번호
         var question = json.data[i].question; //질문
         var value = json.data[i].value;       //점수
         var answer = json.data[i].answer;     //대답
+
         //<div class="${"Q" + n}"></div>
         str += `Q. <strong>${question}</strong>`;
         str += `<div class="${category + n}">`;
@@ -39,10 +22,14 @@ function radio(json) {
         str += '</div>'; //문제마다 줄바꿈//radioBtn[i].checked = true;
   }
 
-  wrap.innerHTML += str;
+  wrap.innerHTML = str;
 }
 
 getUrlData(radio);
+
+// var checked = document.querySelector(`input[name="e1"]:checked`);
+// var genderSValue = genderSRadio ? genderSRadio.value : "";
+// console.log('checked', checked);
 
 function getUrlData(callback) {
   fetch('./data.json')
